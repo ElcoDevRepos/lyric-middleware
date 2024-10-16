9e3e4484fd20e2593eb436502e483035a873219a
2toesxtra <devkelly9622@gmail.com>
2024-10-16 14:44:39 -0400

    create webdoctors patient works

--- services/newPatientHandler/webDoctors/behaviors/webDoctorsRequestSender.js
+++ services/newPatientHandler/webDoctors/behaviors/webDoctorsRequestSender.js
@@ -1,13 +1,59 @@
+const { getCityName } = require("../../../../lib/google/getCity");
 const { sendWebDoctorsAuthRequest } = require("../../../../lib/webDoctors/authRequest")
 
 class WebDoctorsRequestSender {
-    constructor(config) {
-        this.config = config
+    constructor(form) {
+        this.form = form
     }
 
     async sendRequest() {
-        const data = await sendWebDoctorsAuthRequest();
-        console.log(data);
+        const form = this.form;
+
+        let city = form.city;
+        let zip = "";
+        if (form.zip) {
+            zip = form.zip.split("-")[0];
+        }
+        if (!city && zip) {
+            city = await getCityName(zip);
+            city = city.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+        } else if (!city && !zip) {
+            throw new Error("invalid city or zip");
+        }
+
+        const wdVendorId = process.env.ENVIRONMENT == "staging" ? 35 : 18;
+
+        const member = {
+            ID: 0,
+            FirstName: form.firstName,
+            LastName: form.lastName,
+            Email: form.email,
+            VendorId: wdVendorId,
+            Gender: form.gender,
+            DateOfBirth: form.dateOfBirth,
+            PhoneNo: form.phone,
+            Address1: form.address,
+            Address2: form.address2,
+            City: city,
+            Zipcode: zip,
+            AccountType: 2 
+        };
+        member.FirstName = form.firstName.replace(/[-\s]/g, "");
+        member.LastName = form.lastName.replace(/[-\s]/g, "");
+
+        try {
+            const response = await sendWebDoctorsAuthRequest("/api/patient/createpatient", JSON.stringify(member));
+            if(response?.data) {
+                return {id: response.data};
+            }
+        } catch (e) {
+            return {
+                error: {
+                    code: e.response.status,
+                    message: e.response.data
+                }
+            }
+        }
     }
 }
 
